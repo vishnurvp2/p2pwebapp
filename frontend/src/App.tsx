dbf7b1f331b3115f178b396833b509bd67dab6b7
vishnuprasad ranganatha <vishnuprasadranganatha@gmail.com>
2026-06-28 17:41:32 +0530

    landing page and license file updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
+import LandingPage from "./LandingPage";
+
 function App() {
   return (
     <>
-      <h1 className="font-sans text-2xl font-medium tracking-wide">
-        P2P Web App
-      </h1>
-      <p>React + Vite</p>
-      <p>Version 0.0.1</p>
+      <LandingPage />
     </>
   );
 }
