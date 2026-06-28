3fbeb4df4e667a6698be4f82da5a0f0b89125ed2
vishnuprasad ranganatha <vishnuprasadranganatha@gmail.com>
2026-06-28 16:03:52 +0530

    Initial project structure
    - Monorepo created
    - React + Vite frontend
    - Backend folder reserved

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>P2P Web App</h1>
+      <p>React + Vite</p>
+      <p>Version 0.0.1</p>
+    </>
+  );
+}
+
+export default App;
